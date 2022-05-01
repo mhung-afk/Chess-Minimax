c0cdf39ea1d238dde400d7a68aa6ff8c30c1b259
mhung-afk <rairaikiri@gmail.com>
2022-05-01 15:06:56 +0700

    set up initial board

---
+++ ChessEngine.py
@@ -0,0 +1,16 @@
+# handle rules, valid move, log move
+
+class GameState:
+    def __init__(self):
+        self.board = [
+            ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ readme.txt
@@ -0,0 +1,2 @@
+ChessMain: handle user interaction, input, render board
+ChessEngine: handle rules, valid move, log move, change state
